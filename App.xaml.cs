531a8bb9aab40a7c1dc5b0e714fd0627eabb377d
Fred Song <fred@myocchealthrecord.com>
2026-02-16 12:29:03 +1100

    upgrade to .net 10

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,8 +8,11 @@ namespace YoutubeDownloader
             //Register Syncfusion license
             SyncfusionLicenseProvider.RegisterLicense("MDAxQDMyMzIyZTMwMmUzMGZrMFNJK01LMk5jdUc4dzVMYmlJU250V05PQzVWclVOSE5aeFpHMEdhNDg9");
             InitializeComponent();
+        }
 
-            MainPage = new AppShell();
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            return new Window(new AppShell());
         }
     }
 }
